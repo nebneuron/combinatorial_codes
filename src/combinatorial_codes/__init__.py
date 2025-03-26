ddccec1240d5fd3976185ecfe904e2a5b6f1d9ba
Vladimir Itskov <vitskov@users.noreply.github.com>
2025-03-26 16:18:52 -0400

    Update __init__.py
    bug fix

--- src/combinatorial_codes/__init__.py
+++ src/combinatorial_codes/__init__.py
@@ -16,7 +16,6 @@ from .examples import example_dictionary
 __all__ = [
     "CombinatorialCode",
     "WORD_TYPE",
-    "set_word_type",
     "boolean_matrix_to_array_of_words",
     "sizes_of_words",
     "convert_to_boolean_matrix",

9436b0a353f9343021b0953aae81f4a1e7142b90
Vladimir Itskov <vitskov@users.noreply.github.com>
2025-03-26 16:34:42 -0400

    Update __init__.py
    fix

--- src/combinatorial_codes/__init__.py
+++ src/combinatorial_codes/__init__.py
@@ -1,62 +1,41 @@
 # __init__.py
 
-from .tda import homology_is_trivial, compute_homology_from_facets
-from .examples import example_code, example_dictionary
-
-from .code import (
-    CombinatorialCode,
-    WORD_TYPE,
-    boolean_matrix_to_array_of_words,
-    sizes_of_words,
-    convert_to_boolean_matrix,
-    find_maximal_words,
-    x_is_a_subset_of_any_in_List,
-    NumbaList,
-    comb,
-    count_bits,
-    custom_bit_length,
-    intersection_of_codewords_from_bits,
-    empty_list,
-    bit_order,
-    generate_binary_strings,
-    array_of_words_to_boolean_matrix,
-    array_of_words_to_vectors_of_integers,
-    inclusion_relation,
-    link_facets,
-    x_is_a_superset_of_any_in_List,
-    lattice_dictionary_by_size,
-    nerve_of_max_words,
-    simplicial_violators_from_words,
-    Obstructions,
-)
+from .tda import * 
+from .codes import CombinatorialCode
+from .codes import WORD_TYPE
+from .codes import boolean_matrix_to_array_of_words
+from .codes import sizes_of_words
+from .codes import convert_to_boolean_matrix
+from .codes import find_maximal_words
+from .codes import x_is_a_subset_of_any_in_List
+from .codes import NumbaList
+from .codes import comb
+from .codes import count_bits
+from .codes import custom_bit_length
+from .codes import intersection_of_codewords_from_bits
+from .codes import empty_list
+from .codes import bit_order
+from .codes import generate_binary_strings
+from .codes import array_of_words_to_boolean_matrix
+from .codes import array_of_words_to_vectors_of_integers
+from .codes import inclusion_relation
+from .codes import link_facets
+from .codes import x_is_a_superset_of_any_in_List
+from .codes import lattice_dictionary_by_size
+from .codes import nerve_of_max_words
+from .codes import simplicial_violators_from_words
+from .codes import Obstructions
 
 __all__ = [
     "CombinatorialCode",
     "WORD_TYPE",
+    "set_word_type",
     "boolean_matrix_to_array_of_words",
     "sizes_of_words",
     "convert_to_boolean_matrix",
     "find_maximal_words",
     "x_is_a_subset_of_any_in_List",
-    "NumbaList",
-    "homology_is_trivial",
-    "compute_homology_from_facets",
-    "example_code",
-    "example_dictionary",
-    "comb",
-    "count_bits",
-    "custom_bit_length",
-    "intersection_of_codewords_from_bits",
-    "empty_list",
-    "bit_order",
-    "generate_binary_strings",
-    "array_of_words_to_boolean_matrix",
-    "array_of_words_to_vectors_of_integers",
-    "inclusion_relation",
-    "link_facets",
-    "x_is_a_superset_of_any_in_List",
-    "lattice_dictionary_by_size",
-    "nerve_of_max_words",
-    "simplicial_violators_from_words",
-    "Obstructions",
+    "NumbaList", " homology_is_trivial", "compute_homology_from_facets", "example_code", "example_dictionary"
 ]
+
+
